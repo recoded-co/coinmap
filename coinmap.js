ced4e5956075794d39ea60ea41c8ba4d79a38ab8
Pavol Rusnak <stick@gk2.sk>
2013-04-14 02:36:39 +0200

    add toner layer

--- coinmap.js
+++ coinmap.js
@@ -1,9 +1,20 @@
 function coinmap() {
-  var map = L.map('map').setView([0, 0], 3);
 
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+  var tileOSM = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors.',
     maxZoom: 18
+  });
+
+  var tileToner = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+    maxZoom: 18
+  });
+
+  var map = L.map('map', { zoom: 3, layers: [tileOSM] });
+
+  L.control.layers({
+    "OpenStreetMap": tileOSM,
+    "Toner": tileToner,
   }).addTo(map);
 
   map.locate({setView: true, maxZoom: 6});
