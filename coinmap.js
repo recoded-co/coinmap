c2be1145eab8ba18b2128832556c85b084ca53d1
Pavol Rusnak <stick@gk2.sk>
2013-04-14 20:13:53 +0200

    add mapquest aerial

--- coinmap.js
+++ coinmap.js
@@ -1,7 +1,7 @@
 function coinmap() {
 
   var tileOSM = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+    attribution: 'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
     maxZoom: 18
   });
 
@@ -10,17 +10,25 @@ function coinmap() {
     maxZoom: 18
   });
 
-  var tileMapQuest = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+  var tileMapQuest = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
     subdomains: ['otile1','otile2','otile3','otile4'],
     attribution: 'Map tiles by <a href="http://open.mapquestapi.com/">MapQuest</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
     maxZoom: 18
   });
+
+  var tileMapQuestAerial = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+    subdomains: ['otile1','otile2','otile3','otile4'],
+    attribution: 'Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+    maxZoom: 18
+  });
+
 
   var map = L.map('map', { zoom: 3, layers: [tileOSM] });
 
   L.control.layers({
     "OpenStreetMap": tileOSM,
     "MapQuest Open": tileMapQuest,
+    "MapQuest Open Aerial": tileMapQuestAerial,
     "Toner": tileToner,
   }).addTo(map);
 
